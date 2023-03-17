5a8c949373eb8b10c64bca1e52d66788ce174266
clement <clementpaquentin@hotmail.fr>
2023-03-17 23:56:18 +0100

    FEAT - Chat op

---
+++ public/assets/js/socket_view.js
@@ -0,0 +1,142 @@
+let socket = io();
+
+let message = document.querySelector('#listMessage');
+let formSendMessage = document.querySelector('#chatForm');
+let formSetUsername = document.querySelector('#formUsername');
+
+let connected = false;
+
+/**
+ * Container
+ */
+const chatContent = document.querySelector('#chatContent');
+const setUsernameContent = document.querySelector('#setUsernameContent');
+
+/**
+ * toast
+ */
+const toastLiveExample = document.querySelector('#liveToast')
+
+/**
+ * UserInfo
+ */
+let currentUsername = "";
+let userList = [];
+let totalUser = 0;
+
+const init = () => {
+
+    if (connected) {
+        chatContent.className = "d-flex justify-content-between gap-2 mb-5";
+        setUsernameContent.className = "d-none";
+    } else {
+        setUsernameContent.className = "d-block";
+        chatContent.className = "d-none";
+    }
+}
+const setUsername = (username) => {
+    if (username) {
+        currentUsername = username
+        socket.emit('add user', username)
+
+    }
+}
+
+const sendMessage = message => {
+
+    if (message && connected) {
+        socket.emit('new message', message);
+    }
+}
+const setMessage = (msg) => {
+
+    document.querySelector('#chatInput').value = "";
+    let text = "";
+    if (msg.username === currentUsername) {
+        text = `<div class="d-flex flex-column align-items-end">
+                    <h6>Vous</h6>
+                    <div class="w-50 bg-secondary rounded">
+                        <p class="text-white p-1 m-0">${msg.message}</p>
+                    </div>
+                    <h6 style="font-size: 10px" class="text-secondary">${msg.date}</h6>
+                </div>`
+
+    } else {
+        text = `<div>
+                    <h6>${msg.username}</h6>
+                    <div class="w-50 bg-info rounded">
+                        <p class="text-white p-1 m-0">${msg.message}</p>
+                    </div>
+                    <h6 style="font-size: 10px" class="text-secondary">${msg.date}</h6>
+                </div>`
+    }
+
+    message.innerHTML += text;
+}
+
+const getUsersList = (userTab) => {
+    let buffer = ""
+    userTab.map(target => {
+
+        buffer += `<p>${target}</p>`;
+    })
+    document.querySelector('#listUserContainer').innerHTML = buffer;
+    document.querySelector('#totalUser').innerHTML = `${userTab.length} Utilisateur${userTab.length > 1 ? 's connectés' : ' connecté'}`;
+}
+
+//second parameter : true for join and false for left
+const userJoinOrLeft = (user, state) => {
+
+    message.innerHTML += `<p class="text-secondary text-center">${user} a ${state ? "rejoint" : "quitté"} la conversation</p>`;
+}
+
+//FORM SET USERNAME
+formSetUsername.addEventListener('submit', function (e) {
+    e.preventDefault();
+    setUsername(e.target[0].value)
+})
+
+//FORM SEND MESSAGE
+formSendMessage.addEventListener('submit', function (e) {
+    e.preventDefault();
+    sendMessage(e.target[0].value)
+});
+
+
+socket.on('new message', data => {
+    setMessage(data)
+})
+
+// Whenever the server emits 'login'
+socket.on('login', (data) => {
+
+    //check if user have same pseudo
+    if(typeof data.info === 'undefined'){
+        connected = true;
+        totalUser++;
+        init()
+        getUsersList(data.listUser)
+    }else{
+        const toast = new bootstrap.Toast(toastLiveExample)
+        toast.show()
+        document.querySelector('#toastContent').innerHTML = data.info
+    }
+});
+
+// Whenever the server emits 'user joined'
+socket.on('user joined', (data) => {
+
+    userList.push(data.username)
+    getUsersList(data.listUser)
+    userJoinOrLeft(data.username, true)
+
+});
+
+// Whenever the server emits 'user left'
+socket.on('user left', (data) => {
+    totalUser--;
+    getUsersList(data.listUser)
+    userJoinOrLeft(data.username, false)
+});
+
+init();
--- routes/chat.js
+++ routes/chat.js
@@ -2,20 +2,99 @@ import express from "express";
 import {Server} from "socket.io";
 
 const chatRouter = express.Router();
-export default function(httpServer){
+export default function (httpServer) {
 
     const io = new Server(httpServer);
 
-    io.on('connection', (socket) => {
-        console.log('a user connected');
-        socket.on('disconnect', () => {
-            console.log('user disconnected');
-        })
+    let listUser = [];
 
-        socket.on('chat message', function(msg){
-            console.log('message recu '+msg)
-            io.emit('chat message', msg);
-        })
+    io.on('connection', (socket) => {
+        let addedUser = false;
+        // when the client emits 'new message', this listens and executes
+        socket.on('new message', (data) => {
+
+            const currentDate = new Date(Date.now()).toLocaleString()
+
+            // we tell the client to execute 'new message'
+            socket.broadcast.emit('new message', {
+                username: socket.username,
+                message: data,
+                date: currentDate
+            });
+            socket.emit('new message', {
+                username: socket.username,
+                message: data,
+                date: currentDate
+            });
+        });
+
+        // when the client emits 'add user', this listens and executes
+        socket.on('add user', (username) => {
+            console.log(listUser)
+            let userAlreadyExist = false
+            if (listUser.length > 0) {
+                listUser.map(target => {
+                    if(target.toUpperCase() === username.toUpperCase()){
+
+                        userAlreadyExist = true;
+                    }
+                })
+            }
+
+            if(!userAlreadyExist){
+                if (addedUser) return;
+
+                // we store the username in the socket session for this client
+                socket.username = username;
+                addedUser = true;
+                listUser.push(socket.username);
+
+                socket.emit('login', {
+                    username: socket.username,
+                    listUser
+                });
+                // echo globally (all clients) that a person has connected
+                socket.broadcast.emit('user joined', {
+                    username: socket.username,
+                    listUser
+                });
+            }else{
+
+                socket.emit('login', {
+                    info: `Désolé ! Le pseudo <span class="fw-bold">${username}</span> est déjà pris !`
+                });
+            }
+
+
+        });
+
+        // when the client emits 'typing', we broadcast it to others
+        socket.on('typing', () => {
+            socket.broadcast.emit('typing', {
+                username: socket.username
+            });
+        });
+
+        // when the client emits 'stop typing', we broadcast it to others
+        socket.on('stop typing', () => {
+            socket.broadcast.emit('stop typing', {
+                username: socket.username
+            });
+        });
+
+        // when the user disconnects.. perform this
+        socket.on('disconnect', () => {
+            if (addedUser) {
+
+                listUser = listUser.filter(e => e !== socket.username);
+
+                // echo globally that this client has left
+                socket.broadcast.emit('user left', {
+                    username: socket.username,
+                    listUser
+                });
+            }
+        });
 
     });
 
